e4b8ac2d0ca0c076083735d7363f63357e578029
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-06-04 22:37:07 +0200

    using canvas, after all..

--- src/js/jsube.js
+++ src/js/jsube.js
@@ -15,20 +15,39 @@ cells[0][1] = Ube.EMPTY;
 cells[1][0] = Ube.WALKABLE;
 cells[1][1] = Ube.OUT;
 
-$(document).ready(function(){
+var g_cell_images = {}
+var g_ctx;
+
+function load_image(dest, id, url, callback) {
+    var img = new Image();
+    img.onload = function() {
+	g_cell_images["1"] = img;
+	callback();
+    }
+    img.src = url;
+    img.width = 32;
+    img.height = 32;
+    
+}
+
+function load_images(callback) {
+    load_image(g_cell_images, "1", "../../data/images/png/cell_1.png", callback);
+}
+
+function drawPuzzle() {
     for (i = 0 ; i < 22 ; i++) {
 	for (j = 0 ; j < 11 ; j++) {
 	    // Add a div to the main div
 	    if (cells[i][j] != null) {
 		if (cells[i][j] == Ube.WALKABLE) {
-		    div = document.createElement("div");
-		    div.setAttribute("class", "cell");
-		    div.setAttribute("position", "absolute");
-		    div.setAttribute("top", i*32);
-		    div.setAttribute("left", j*32);
-		    $('#playground').append(div);
+		    g_ctx.drawImage(g_cell_images["1"], j*32, i*32, 32, 32);
 		}
 	    }
 	}
     }
+}
+
+$(document).ready(function(){
+    g_ctx = document.getElementById('playground').getContext('2d');
+    load_images(drawPuzzle);
 });
