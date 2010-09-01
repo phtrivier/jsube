280f06b4f5ab304555a39023df6773de1424f187
Pierre-Henri Trivier <phtrivier@yahoo.fr>
2010-09-01 21:59:44 +0200

    Hover and click styling ... god does CSS suck...

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,29 @@
-function load_tutorial_level_list() {
-    var lg = get_lg();
+g_home_i18n = {
+    "en" : { "level.choose" : "Choose a level",
+	     "level.preview" : "Level preview",
+	     "play" : "Play"   },
+    "fr" : { "level.choose" : "Choisissez un niveau",
+	     "level.preview" : "Aperçu du niveau",
+	     "play" : "Jouer"  } 
+};
+
+function load_translated_text(lg) {
+    $('#levels-header').append(g_home_i18n[lg]['level.choose']);
+    $('#levels-header').mousedown(function (event) {
+	if (event.preventDefault) {
+	    event.preventDefault();
+	}
+    });
+}
+
+function on_level_selected(li) {
+    $("#levels > li").each(function (index) {
+	$(this).removeClass('selected-level');
+    });
+    li.removeClass('hovered-level').addClass('selected-level')
+}
+
+function load_tutorial_level_list(lg) {
     $("#levels").empty();
     for (var i = 0 ; i < 7 ; i++) {
 
@@ -7,14 +31,24 @@ function load_tutorial_level_list() {
 	levelDiv.append(PUZZLE_STRUCTS[i].title[lg]);
 	
 	levelDiv.hover(function (event) {
-	    $(this).addClass('hovered-level');
+	    if (!$(this).hasClass('selected-level')) {
+		$(this).addClass('hovered-level');
+	    }
 	}, 
 		       function(event) {
 			   $(this).removeClass('hovered-level');
 		       });
 	
-	// TODO(levelDiv.click(on_link_clicked(), load_level_preview(i));
+	levelDiv.click(function (event) {
+	    on_level_selected($(this));
+	});
 	
+	levelDiv.mousedown(function (event) {
+	    if (event.preventDefault) {
+		event.preventDefault();
+	    }
+	});
+
 	$("#levels").append(levelDiv);
     }
 }
@@ -25,7 +59,9 @@ $(document).ready(function(){
     var p = new Puzzle(PUZZLE_STRUCTS[2]);
    
     drawer.load_images(function () {
-	load_tutorial_level_list();
+	var lg = get_lg();
+	load_translated_text(lg);
+	load_tutorial_level_list(lg);
 	// drawer.draw_puzzle(p);
     });
 
